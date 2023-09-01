2aadcc28c2d4cad0ff1b68ad16bd112d504ec812
Zhenliang He <lynn_ho@163.com>
2023-09-01 23:22:46 +0800

    Update make_workspace.sh

--- make_workspace.sh
+++ make_workspace.sh
@@ -30,9 +30,7 @@ ANACONDA_HOME=$HOME/ProgramFiles/anaconda3
 # step 1.1:install minconda
 backup_file_or_dir $ANACONDA_HOME
 
-if [ ! -e ./miniconda.sh ]; then
-    wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
-fi
+wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
 bash ./miniconda.sh -b -p $ANACONDA_HOME
 . $ANACONDA_HOME/bin/activate
 
