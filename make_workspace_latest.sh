2515755f403fa4468a11dfc845b899ea3b33e17c
Zhenliang He <lynn_ho@163.com>
2023-10-18 10:36:12 +0800

    Update make_workspace_latest.sh

--- make_workspace_latest.sh
+++ make_workspace_latest.sh
@@ -42,7 +42,7 @@ bash ./miniconda.sh -b -p $ANACONDA_HOME
 
 # step 1.2: install mamba
 cd $ANACONDA_HOME/lib; ln -s libarchive.so libarchive.so.13; cd -
-$ANACONDA_HOME/bin/conda install -y -c conda-forge -c nodefaults mamba
+$ANACONDA_HOME/bin/conda install -y -c conda-forge mamba
 
 
 # step 1.3: install tools
