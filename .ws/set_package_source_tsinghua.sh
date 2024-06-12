acdade9b008acf2bad25242e86c8c164d39538a4
Zhenliang He <lynn_ho@163.com>
2024-06-12 09:53:37 +0800

    Create set_package_source_tsinghua.sh

---
+++ .ws/set_package_source_tsinghua.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# conda: conda-forge as default
+echo 'channels:
+  - conda-forge
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  deepmodeling: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/' > ~/.condarc
+conda clean -i -y
+mamba clean -i -y
+
+# pip
+mkdir -p ~/.config/pip
+
+echo '[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+' > ~/.config/pip/pip.conf
