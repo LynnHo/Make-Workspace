425d8dde3fca7e38aafb06d34eff645ef6bbd8d5
Zhenliang He <lynn_ho@163.com>
2025-07-15 21:47:39 +0800

    Create get_tmux_pane-border.sh

---
+++ .ws/get_tmux_pane-border.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pid=$(pgrep -P $1 | head -1)
+if [ -z "$pid" ]; then
+    echo " [ $(date '+%Y/%m/%d %H:%M') ] "
+fi
+
+cmd=$(ps -o cmd --no-headers --pid $pid)
+etime=$(ps -o etime --no-headers --pid $pid | awk '{print $1}')
+if [ -n "$cmd" ]; then
+    echo " PID: $1 • ETIME: $etime >> $cmd "
+fi
